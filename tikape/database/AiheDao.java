25336996794e5dc78ef84baeed60a4f45700af39
Susanna Hällsten <susanna.hallsten@helsinki.fi>
2017-06-11 20:57:27 +0300

    enpä tiiä onnistuuko tää

---
+++ tikape/database/AiheDao.java
@@ -0,0 +1,45 @@
+
+package tikape.database;
+
+import java.sql.SQLException;
+import java.util.List;
+import tikape.collectors.AiheCollector;
+import tikape.pojo.Aihe;
+
+
+
+public class AiheDao implements Dao<Aihe, Integer>{
+    
+    private Database database;
+
+    public AiheDao(Database database) {
+        this.database = database;
+    }
+
+    @Override
+    public Aihe findOne(Integer key) throws SQLException {
+        List<Aihe> aiheet = this.database.queryAndCollect("SELECT * FROM Aihe WHERE id = ?", new AiheCollector(), key);
+        if (aiheet.isEmpty()) {
+            return null;
+        }
+
+        return aiheet.get(0);
+    }
+
+    @Override
+    public void save(Aihe aihe) throws SQLException {
+        this.database.update("INSERT INTO Aihe (nimi, luomispaiva, alue) VALUES (?, ?, ?)", aihe.getNimi(), aihe.getLuomispaivamaara(), aihe.getAlue());
+    }
+
+    @Override
+    public List<Aihe> findAll() throws SQLException {
+        return this.database.queryAndCollect("SELECT * FROM Aihe", new AiheCollector());
+    }
+
+    @Override
+    public void delete(Integer key) throws SQLException {
+        this.database.update("DELETE FROM Aihe WHERE id = ?", key);
+    }
+
+    
+}
---
+++ tikape/database/ViestiDao.java
@@ -0,0 +1,44 @@
+
+package tikape.database;
+
+import tikape.database.Dao;
+import tikape.database.Database;
+import java.sql.SQLException;
+import java.util.List;
+import tikape.collectors.ViestiCollector;
+import tikape.pojo.Viesti;
+
+public class ViestiDao implements Dao<Viesti, Integer>{
+    
+    private Database database;
+
+    public ViestiDao(Database database) {
+        this.database = database;
+    }
+
+    @Override
+    public Viesti findOne(Integer key) throws SQLException {
+        List<Viesti> viestit = this.database.queryAndCollect("SELECT * FROM Viesti WHERE id = ?", new ViestiCollector(), key);
+        if (viestit.isEmpty()) {
+            return null;
+        }
+
+        return viestit.get(0);
+    }
+
+    @Override
+    public void save(Viesti viesti) throws SQLException {
+        this.database.update("INSERT INTO Viesti (lahettaja, sisalto, aihe, alue) VALUES (?, ?, ?, ?)", viesti.getLahettaja(), viesti.getSisalto(), viesti.getAihe(), viesti.getAlue());
+    }
+
+    @Override
+    public List<Viesti> findAll() throws SQLException {
+        return this.database.queryAndCollect("SELECT * FROM Viesti", new ViestiCollector());
+    }
+
+    @Override
+    public void delete(Integer key) throws SQLException {
+        this.database.update("DELETE FROM Viesti WHERE id = ?", key);
+    }
+    
+}
